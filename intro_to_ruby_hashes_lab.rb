75671b7593f0a724621d82923fc39d61e2ffd5ab
Danny Rooker <dannyrooks@gmail.com>
2019-07-11 22:42:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,9 +36,9 @@ def monopoly_with_fourth_tier
 	  railroads: {
 	    pieces: 4,
 	    names: {
-	      reading_railroad: {
+	      :reading_railroad=> {}
 	        "mortgage_value": "$100"
-	      }
+	      }}
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
