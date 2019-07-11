00d302d5adfdafb0857c1fd5588b1a25b9c326eb
Danny Rooker <dannyrooks@gmail.com>
2019-07-11 22:42:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_fourth_tier
 	    names: {
 	      :reading_railroad=> {}
 	        "mortgage_value": "$100"
-	      }}
+	      }
 	    },
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
