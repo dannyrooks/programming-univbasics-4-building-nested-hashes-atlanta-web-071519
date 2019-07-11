ebc8697002a118e6299c8bc296d797e1c8d26942
Danny Rooker <dannyrooks@gmail.com>
2019-07-11 22:47:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,6 +24,7 @@ def monopoly_with_third_tier
 	      reading_railroad: {},
 	    
 	    pensylvania_railroad: {}
+	    }
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
